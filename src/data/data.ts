71d88b248ad6dfb903fd7a520a86fbfcc266ee33
andrew9488 <pashkevichandrew@gmail.com>
2021-06-09 14:51:23 +0300

    feat: create data structure

---
+++ src/data/data.ts
@@ -0,0 +1,28 @@
+export const data = [
+    {
+        name: "Root", key: "0", type: "folder", children: [
+            {
+                name: "Folder_1", key: "0-1", type: "folder", children: [
+                    {name: "file_1", key: "0-1-1", type: "file", content: "File is empty"}
+                ]
+            },
+            {
+                name: "Folder_2", key: "0-2", type: "folder", children: [
+                    {
+                        name: "Folder_1", key: "0-2-1", type: "folder", children: [
+                            {name: "file_1", key: "0-2-1-1", type: "file", content: "File is empty"},
+                            {name: "file_1", key: "0-2-1-2", type: "file", content: "File is empty"}
+                        ]
+                    },
+                    {
+                        name: "Folder_2", key: "0-2-1", type: "folder", children: []
+                    }
+
+                ]
+            }
+
+        ]
+    }
+]
+
+export type DataType = typeof data
