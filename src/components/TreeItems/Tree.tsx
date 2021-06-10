49be74c4d50622c7932483ac934b90c0c439c5cf
andrew9488 <pashkevichandrew@gmail.com>
2021-06-10 14:36:53 +0300

    fix: fix bug

--- src/components/TreeItems/Tree.tsx
+++ src/components/TreeItems/Tree.tsx
@@ -4,7 +4,7 @@ import {DataType} from "../../data/data";
 import style from "./Tree.module.css";
 
 type TreePropsType = {
-    data: DataType |  {name: string, key: string, type: string, children: {name: string, key: string, type: string, children: ({name: string, key: string, type: string, content: string, children?: undefined} | {name: string, key: string, type: string, children: {}[], content?: undefined})[]}[]}[]
+    data: DataType
 }
 
 export const Tree: React.FC<TreePropsType> = ({data = []}) => {
